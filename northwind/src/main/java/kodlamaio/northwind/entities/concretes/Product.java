2367d133665d6bcad43104681fd52ef8c0a708c1
kubrasnmez <sonmezkbra@gmail.com>
2021-05-27 21:53:41 +0300

    Category and productDao

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -25,8 +27,8 @@ public class Product {
 	@Column(name="product_id")
 	private int id;
 	
-	@Column(name="category_id")
-	private int categoryId;
+	//@Column(name="category_id")
+	// private int categoryId;
 	
 	@Column(name="product_name")
 	private String productName;
@@ -39,6 +41,10 @@ public class Product {
 	
 	@Column(name="quantity_per_unit")
 	private String quantityPerUnit;
+	
+	@ManyToOne()
+	@JoinColumn(name="category_id")
+	private Category category;
 	
 
 }
