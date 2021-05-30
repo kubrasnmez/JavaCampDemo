f1312ce8affac8c55e2f7efb68145eaf5273b2cf
kubrasnmez <sonmezkbra@gmail.com>
2021-05-31 00:34:15 +0300

    fluentValidation

---
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/concretes/dtos/ProductWithCategoryDto.java
@@ -0,0 +1,15 @@
+package kodlamaio.northwind.dataAccess.concretes.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data 
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductWithCategoryDto {
+	private int id;
+	private String productName;
+	private String categoryName;
+
+}
